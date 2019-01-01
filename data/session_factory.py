9798d7c5f333e132e50527b8883b75fc6afe4e4d
John Barksdale <jdbarks@gmail.com>
2018-12-31 19:03:30 -0600

    added database connetions

---
+++ data/session_factory.py
@@ -0,0 +1,30 @@
+import sqlalchemy
+import sqlalchemy.orm
+import db.db_folder as db_folder
+from models.model_base import ModelBase
+# TODO: add all models to be imported here
+# noinspection PyUnresolvedReferences
+from models import move, player
+
+__factory = None
+
+
+def global_init():
+    global __factory
+
+    full_file = db_folder.get_db_folder('hilo.sqlite')
+    conn_str = 'sqlite:///'
+
+    engine = sqlalchemy.create_engine(conn_str, echo=False)
+    ModelBase.metadata.create_all(engine)
+
+    __factory = sqlalchemy.orm.sessionmaker(bind=engine)
+
+
+def create_session():
+    global __factory
+
+    if __factory is None:
+        global_init()
+
+    return __factory()
