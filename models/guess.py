a0c7f74357c84957ed75646873a4fea003993b3d
John Barksdale <jdbarks@gmail.com>
2019-01-02 14:42:32 -0600

    Refactoring for pep8

--- models/guess.py
+++ models/guess.py
@@ -9,12 +9,15 @@ from models.model_base import ModelBase
 class Guess(ModelBase):
     __tablename__ = 'guesses'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    created = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now, index=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True,
+                           autoincrement=True)
+    created = sqlalchemy.Column(sqlalchemy.DateTime,
+                                default=datetime.datetime.now, index=True)
     game_id = sqlalchemy.Column(sqlalchemy.String, index=True)
     guess = sqlalchemy.Column(sqlalchemy.Integer, index=True)
     guess_count = sqlalchemy.Column(sqlalchemy.Integer, index=True)
     player_id = sqlalchemy.Column(sqlalchemy.Integer, index=True)
     is_hi = sqlalchemy.Column(sqlalchemy.Boolean, index=True, default=False)
-    is_correct_guess = sqlalchemy.Column(sqlalchemy.Boolean, index=True, default=False)
+    is_correct_guess = sqlalchemy.Column(sqlalchemy.Boolean, index=True,
+                                         default=False)
     the_number = sqlalchemy.Column(sqlalchemy.Integer, index=True)
--- models/player.py
+++ models/player.py
@@ -9,6 +9,8 @@ from models.model_base import ModelBase
 class Player(ModelBase):
     __tablename__ = 'players'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    created = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True,
+                           autoincrement=True)
+    created = sqlalchemy.Column(sqlalchemy.DateTime,
+                                default=datetime.datetime.now)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
