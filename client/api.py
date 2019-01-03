9e4206b0eba62c11633d9fc5a6f3cf8586d4b454
John Barksdale <jdbarks@gmail.com>
2019-01-03 15:23:04 -0600

    Started work on client app.  Moved views file to web directory

---
+++ client/api.py
@@ -0,0 +1,34 @@
+import uplink
+
+
+class GameService(uplink.Consumer):
+    def __init__(self):
+        super().__init__(base_url='http://localhost:5000')
+
+    @uplink.put('api/game/users')
+    def create_user(self, **kwargs: uplink.Body):
+        pass
+
+    @uplink.get('api/game/users/{username}')
+    def find_user(self, username):
+        pass
+
+    @uplink.post('api/game/game')
+    def create_game(self, **kwargs: uplink.Body):
+        pass
+
+    @uplink.get('api/game/{game_id}/status')
+    def game_status(self, game_id):
+        pass
+
+    @uplink.get('api/users/{user}/top_scores')
+    def player_top_scores(self, user):
+        pass
+
+    @uplink.get('api/game/top_scores')
+    def top_scores(self):
+        pass
+
+    @uplink.post('api/game/play_round')
+    def play_round(self, **kwargs: uplink.Body):
+        pass
---
+++ client/game_app.py
@@ -0,0 +1,33 @@
+from api import GameService
+
+
+def main():
+    svc = GameService()
+
+    print("Game app! (client)")
+    print()
+    print()
+    print("TOP SCORES")
+    for s in svc.top_scores().json():
+        print(f'{s.get("player").get("name")} scored {s.get("score")}')
+    print()
+
+    game_id = svc.create_game().json().get('game_id')
+
+    player_name = input('What is your name? ')
+
+    player = svc.find_user(player_name)
+    if not player:
+        svc.create_user(user=player_name)
+        player = svc.find_user(player_name)
+
+    print(player.json())
+
+    # is_over = False
+    # while not is_over:
+    #     name = player.get('name')
+    #     guess = input('Guess a number: ')
+    #     rnd = svc.play_round(game_id=game_id, user)
+
+if __name__ == "__main__":
+    main()
